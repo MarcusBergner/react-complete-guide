a9721414fd56f43c7ea74f769d1fc8d61a84faa1
Marcus Bergner <marcus.bergner@saxsys.de>
2020-03-09 13:50:49 +0100

    Abschnitt3.34-Working with Components & Re-Using Them

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function name should be begin with lowercase 
+const person = () =>{
+    // AroowFunction: es6 equivalennt to function(){}
+    return <p>I'm a Person !</p>
+};
+
+export default person;
