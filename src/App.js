db0198237a48e1f1cc5bb8db99c5da7861c7317e
Marcus Bergner <marcus.bergner@saxsys.de>
2020-03-09 15:52:26 +0100

    44/Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from "./Person/Person";
-class App extends Component {
-  // define a spezial propertie(state), this only works in components which are extending component!
-  // with React 16.8 a new feature "React hooks" --> manage state also in functional components
-  state = {
+// this show the traditional set up for manage state  by class-components, befor React 16.8 lunched!
+const app = props => {
+ const [personsState, setPersonsState] =  useState({
     persons:[
       {name: "Max", age:28},
       {name: "Manu", age :22},
       {name: "Stefan", age :42}
     ],
     otherState:"some other value"
-  }
-  switchNameHandler =()=>{
+  });
+  const [otherState, setOtherState] = useState( "some other value");
+
+  console.log(personsState,otherState);
+  const switchNameHandler =()=>{
     // console.log("Was clicked!");
-    // DON'T DO THIS: this.state.persons[0].name= "Maximilaim";
+    // DON'T DO THIS: personsState.persons[0].name= "Maximilaim";
     // setState({}) it will merge this with existing data  
-    this.setState({persons:[
+    setPersonsState({persons:[
       {name: "Maximilian", age:28},
       {name: "Manu", age :22},
       {name: "Stefan", age :26}
     ]
-
-    })
-  }
-  render() {
+    });
+  };
     return (
       <div className="App">
        <h1>Hi i'am an react app</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <button onClick={switchNameHandler}>Switch Name</button>
        {/* <Person name="Max" age="22"/> */}
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
+       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies: Racing</Person>
       </div>
     );
+  
   }
-}
-export default App;
+  export default app;
+    // define a spezial propertie(state), this only works in components which are extending component!
+  // with React 16.8 a new feature "React hooks" --> manage state also in functional components
+  // state = {
+  //   persons:[
+  //     {name: "Max", age:28},
+  //     {name: "Manu", age :22},
+  //     {name: "Stefan", age :42}
+  //   ],
+  //   otherState:"some other value"
+  // };
+  
 
