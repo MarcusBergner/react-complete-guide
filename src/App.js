b096fb4b999bd0ddb519ddf0bcf2994cdb0083f8
Marcus Bergner <marcus.bergner@saxsys.de>
2020-03-09 14:30:06 +0100

    38/Understand & Use State

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person";
 class App extends Component {
+  // define a spezial propertie(state), this only works in components which are extending component!
+  // with React 16.8 a new feature "React hooks" --> manage state also in functional components
+  state = {
+    persons:[
+      {name: "Max", age:28},
+      {name: "Manu", age :22},
+      {name: "Stefan", age :42}
+    ]
+  }
   render() {
     return (
       <div className="App">
        <h1>Hi i'am an react app</h1>
        <p>This is really working!</p>
-       <Person name="Max" age="22"/>
-       <Person name="Peter" age="32" />
-       <Person name="Ingo" age="26">My Hobbies: Racing</Person>
+       <button>Switch Name</button>
+       {/* <Person name="Max" age="22"/> */}
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Racing</Person>
       </div>
     );
   }
