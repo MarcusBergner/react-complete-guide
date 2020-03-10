01f383d681681231ae68af625b600d5ca61699ca
Marcus Bergner <marcus.bergner@saxsys.de>
2020-03-10 11:03:30 +0100

    49/working with InlineStyles

--- src/App.js
+++ src/App.js
@@ -33,13 +33,23 @@ class  App extends Component {
     })
   }
   render() {
+    const style = {
+      backgroundColor:"white",
+      font:"inherit",
+      border:"1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
        <h1>Hi i'am an react app</h1>
        <p>This is really working!</p>
        {/* anonymous function will executed onClick 
        returns result of switchNameHandler() */}
-       <button onClick={()=> this.switchNameHandler("Maximilian")}>Switch Name</button>
+       <button
+       style={style} 
+       onClick={()=> this.switchNameHandler("Maximilian")}>Switch Name</button>
        {/* <Person name="Max" age="22"/> */}
        <Person
         name={this.state.persons[0].name}
@@ -47,17 +57,18 @@ class  App extends Component {
        <Person 
        name={this.state.persons[1].name}
         age={this.state.persons[1].age}
-        />
+        click={this.switchNameHandler.bind(this, "MaX !")}
+        changed={this.nameChangedHandler}> My Hobbies: Racing</Person>
        <Person 
        name={this.state.persons[2].name}
         age={this.state.persons[2].age}
-        click={this.switchNameHandler.bind(this, "MaX !")}
-        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+       />
       </div>
     );
   
   }
-  // define a spezial propertie(state), this only works in components which are extending component!
+}
+ // define a spezial propertie(state), this only works in components which are extending component!
   // with React 16.8 a new feature "React hooks" --> manage state also in functional components
   // state = {
     //   persons:[
@@ -66,7 +77,7 @@ class  App extends Component {
       //     {name: "Stefan", age :42}
       //   ],
       //   otherState:"some other value"
-      // };
-      
-    }
+    // }; 
+    
     export default App;
+    
