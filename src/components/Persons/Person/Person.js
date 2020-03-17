b7b846a62854253df33a23448e9c4137061950d8
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-17 06:20:19 +0100

    104. Using React.Fragment in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import classes from "./Person.css";
 import Aux from "../../../hoc/Auxillary";
 // StyleDiv returned a react-component
@@ -16,7 +16,10 @@ class Person extends Component {
                 // return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
                 return (
                     // <div className="Person" style={style}>
-                   <Aux>
+                    // <Fragment>
+                         <Aux> 
+                    {/*alternative to our "AUX" custom High-order-Component:
+                       --> React.Fragment*/}
                    {/* <div className={classes.Person}> */}
 
         <p onClick={this.props.click}>I'm {this.props.name}  and I am {this.props.age}  years old!</p>
@@ -24,7 +27,8 @@ class Person extends Component {
         <input type="text" onChange={this.props.changed} value={this.props.name} />
         {/* </div> */}
         {/* </div> */}
-        </Aux>
+         </Aux> 
+        // </Fragment>
     )
 }
 };
