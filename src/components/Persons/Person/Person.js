0085f5f65d5cdc690b69f5c171fbf860f00e7062
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-18 07:42:26 +0100

    110. using Refs after react 16.3 release!

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,8 +10,15 @@ import PropTypes from "prop-types";
 //-----notes-----
 // for add some prop types --> use an extra react-package, "npm install --save prop-types"
 class Person extends Component {
+
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.inputElement.focus();
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
   }
   render() {
     console.log("[Person.js] randering...");
@@ -35,7 +42,8 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
-          ref={(inputEl) => { this.inputElement = inputEl }}
+          // ref={(inputEl) => { this.inputElement = inputEl }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
